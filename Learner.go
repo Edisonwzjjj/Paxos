218518367b390524cfde6a1118bfc8adf32a5f67
Edisonwzjjj <126035354+Edisonwzjjj@users.noreply.github.com>
2023-03-01 10:23:22 +0000

    update Learner && Proposer

--- Learner.go
+++ Learner.go
@@ -1,7 +1,65 @@
+package paxos
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/rpc"
+)
+
+
 type Learner struct {
 	lis net.listener
 
 	id int
 
 	acceptedMsg map[int]MsgArgs
+}
+
+
+func (l * Learner) Learn (args *MsgArgs, reply *MsgReply) error {
+	a := l.acceptedMsg[args.From]
+	if a.Number < args.Number {
+		l.acceptedMsg[args.From] = *args
+		reply.Ok = true
+	} else {
+		reply.Ok = false
+	}
+	return nil
+
+}
+
+func (l *Learner) chosen() interface{} {
+	acceptCount := make(map[int]int)
+	acceptMsg := make(map[int]MsgArgs)
+
+	for _, accepted := range l.acceptedMsg {
+		if accepted.Number != 0 {
+			acceptCount[accepted.Number]++
+			acceptMsg[accepted.Number] = accepted
+
+		}
+
+	}
+
+	for n, count := range acceptCount {
+		if count >= l.majority() {
+			return acceptMsg[n].Value
+		}
+	}
+	return nil
+}
+
+
+func (l* Learner) majority() int {
+	return len(l.acceptedMsg) / 2 + 1
+}
+
+
+func newLearner(id int , acceptorIds []int) *Learner {
+	Learner := &Learner{
+		id : id,
+		acceptedMsg: make(map[int]MsgArgs), 
+	}
+	for _, aid := range 
 }
--- Proposer.go
+++ Proposer.go
@@ -1,3 +1,9 @@
+package paxos
+
+import "fmt"
+
+
+
 type Proposer struct {
 	//服务器id
 	id int
@@ -5,5 +11,74 @@ type Proposer struct {
 	round int
 	//提案编号
 	number int
-	acceptor []int
+	acceptors []int
+}
+
+func (p *Proposer) propose(v interface{}) interface{} {
+	p.round++
+	p.number = p.proposalNumber()
+
+	//phase 1
+	prepareCount := 0
+	maxNumber := 0
+	for _, aid := range p.acceptors {
+		args := MsgArgs {
+			Number : p.number , 
+			From : p.id ,
+			To : aid ,
+		}
+		reply := new(MsgReply) 
+		err := call(fmt.Sprintf("127.0.0.1 : %d", aid), "Acceptor.Prepare", args, reply)
+		if !err {
+			continue
+		}
+		if reply.Ok {
+			prepareCount++
+			if reply.number > maxNumber {
+				maxNumber = reply.number
+				v = reply.Value
+			}
+		}
+		if prepareCount == p.majority() {
+			break
+		}
+	}
+
+	//phase 2
+	acceptCount := 0
+	if prepareCount >= p.majority() {
+		for _, aid := range p.acceptors {
+			args := MsgArgs{
+				Number: p.number,
+				Value: v,
+				From: p.id,
+				To: aid,
+			}
+			reply := new(MsgReply)
+			ok := call(fmt.Sprintf("127.0.0.1:%d", aid), "Acceptor.Accept", args, reply)
+			if !ok {
+				continue
+			}
+			
+			if reply.Ok {
+				acceptCount++
+			}
+		}
+	}
+	
+	if acceptCount >= p.majority() {
+		// 选择的提案的值
+		return v
+	}
+	return nil
+}
+
+
+func (p *Proposer) majority() int {
+	return len(p.acceptors) / 2 + 1
+}
+
+// 提案编号 = (轮次, 服务器 id)
+func (p *Proposer) proposalNumber() int {
+	return p.round << 16 | p.id
 }
